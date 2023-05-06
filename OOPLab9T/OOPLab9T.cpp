a179509199a63e3e51b77ad170c750230be06f01
Miracle179 <prsky.ivan@gmail.com>
2023-05-06 20:57:33 +0200

    Створено функцію-шаблон sequentialSearch, яка приймає масив, його розмір та ключ для пошуку. Функція проходиться по масиву і порівнює кожен елемент з ключем. Якщо елемент знайдено, індекс оновлюється до поточного значення ітератора.
    Також є спеціалізація функції-шаблону для типу char*, оскільки порівняння рядків за допомогою оператора == не дає очікуваного результату. Слід використовувати функцію std::strcmp().

--- OOPLab9T/OOPLab9T.cpp
+++ OOPLab9T/OOPLab9T.cpp
@@ -1,20 +1,48 @@
-﻿// OOPLab9T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstring>
 
-#include <iostream>
+template<typename T>
+int sequentialSearch(T arr[], int size, T key) {
+    int lastIndex = -1;  // індекс останнього знайденого елемента, за замовчуванням -1
 
-int main()
-{
-    std::cout << "Lab №9. STL. \n";
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {  // якщо елемент знайдено
+            lastIndex = i;    // оновити індекс останнього знайденого елемента
+        }
+    }
+
+    return lastIndex;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+// Спеціалізація для типу char*
+template<>
+int sequentialSearch(char* arr[], int size, char* key) {
+    int lastIndex = -1;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    for (int i = 0; i < size; i++) {
+        if (std::strcmp(arr[i], key) == 0) {  // порівняти два рядки за допомогою std::strcmp()
+            lastIndex = i;
+        }
+    }
+
+    return lastIndex;
+}
+
+int main() {
+    int intArr[] = {1, 2, 3, 4, 5};
+    int intKey = 3;
+    std::cout << "Index of last occurrence of " << intKey << " in intArr: "
+              << sequentialSearch(intArr, 5, intKey) << std::endl;
+
+    double doubleArr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    double doubleKey = 2.2;
+    std::cout << "Index of last occurrence of " << doubleKey << " in doubleArr: "
+              << sequentialSearch(doubleArr, 5, doubleKey) << std::endl;
+
+    char* strArr[] = {"apple", "banana", "orange", "grape", "apple"};
+    char* strKey = "apple";
+    std::cout << "Index of last occurrence of \"" << strKey << "\" in strArr: "
+              << sequentialSearch(strArr, 5, strKey) << std::endl;
+
+    return 0;
+}
