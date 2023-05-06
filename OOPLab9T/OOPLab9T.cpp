9164b1d98d2e30c0d40b7155bf16185b7091b32f
Miracle179 <prsky.ivan@gmail.com>
2023-05-06 21:31:22 +0200

    Є шаблон функції для впорядкування масиву методом швидкого впорядкування за зростанням для типу char*. Функція отримує вказівник на початок масиву, індекс лівої границі та індекс правої границі масиву. Алгоритм швидкого впорядкування розділяє масив на дві частини, ті, які менші за опорний елемент, та ті, які більші за нього, і рекурсивно викликає саму себе для обох підмасивів.

--- OOPLab9T/OOPLab9T.cpp
+++ OOPLab9T/OOPLab9T.cpp
@@ -1,5 +1,8 @@
 ﻿#include <iostream>
 #include <cstring>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 template<typename T>
 int sequentialSearch(T arr[], int size, T key) {
@@ -28,6 +31,37 @@ int sequentialSearch(char* arr[], int size, char* key) {
     return lastIndex;
 }
 
+
+// шаблон функції для впорядкування методом швидкого впорядкування
+template<typename T>
+void quickSort(T* arr, int left, int right) {
+    int i = left, j = right;
+    T tmp;
+    T pivot = arr[(left + right) / 2];
+
+    /* розділення */
+    while (i <= j) {
+        while (std::strcmp(arr[i], pivot) < 0)
+            i++;
+        while (std::strcmp(arr[j], pivot) > 0)
+            j--;
+        if (i <= j) {
+            tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
+        }
+    };
+
+    /* рекурсивний виклик */
+    if (left < j)
+        quickSort(arr, left, j);
+    if (i < right)
+        quickSort(arr, i, right);
+}
+
+
 int main() {
     int intArr[] = {1, 2, 3, 4, 5};
     int intKey = 3;
@@ -43,6 +77,64 @@ int main() {
     char* strKey = "apple";
     std::cout << "Index of last occurrence of \"" << strKey << "\" in strArr: "
               << sequentialSearch(strArr, 5, strKey) << std::endl;
+
+    
+    // введення даних з клавіатури
+    std::cout << "Enter the number of elements: ";
+    int n;
+    std::cin >> n;
+    char** arr = new char*[n];
+    std::cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        arr[i] = new char[100];
+        std::cin >> arr[i];
+    }
+
+    // виведення несортованого масиву
+    std::cout << "Unsorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // виклик функції quickSort
+    quickSort(arr, 0, n - 1);
+
+    // виведення впорядкованого масиву
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // випадкові дані
+    srand(time(NULL));
+    int size = 10;
+    char** arr_random = new char*[size];
+    for (int i = 0; i < size; i++) {
+        arr_random[i] = new char[100];
+        for (int j = 0; j < 10; j++) {
+            arr_random[i][j] = (rand() % 26) + 'a';
+        }
+        arr_random[i][10] = '\0';
+    }
+
+    // виведення несортованого масиву
+    std::cout << "Unsorted random array: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr_random[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // виклик функції quickSort
+    quickSort(arr_random, 0, size - 1);
+
+    // виведення впорядкованого масиву
+    std::cout << "Sorted random array: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr_random[i] << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
